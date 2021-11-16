40efb336aadfee3e9ec003dc273abdc5b579942d
fengyuchao <13241484699@163.com>
2021-11-16 18:14:44 +0800

    feat: update

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import eslintPlugin from 'vite-plugin-eslint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    // eslintPlugin()//本地开发和构建的时候进行规范校验
+  ] 
 })
